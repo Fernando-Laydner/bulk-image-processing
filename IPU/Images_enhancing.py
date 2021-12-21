f3b7c1524746122bbea7587925a5b8f55a2a600a
Fernando-Laydner <83661575+Fernando-Laydner@users.noreply.github.com>
2021-12-21 08:46:43 -0300

    Add files via upload

--- IPU/Images_enhancing.py
+++ IPU/Images_enhancing.py
@@ -2,25 +2,42 @@ from PIL import Image
 from shutil import copyfile
 import os
 
+# Known extensions not supported: exr, pfm, sfw, x3f, fts, hdr, mng, pam, picon, pict, wpg, xcf, xpm, xwd, svg, wbmp,
+# cr2, erf, heic, nrw, orf, pef, pes, ref, rw2
+
+
 # Convert and tries to enhance the image.
-def enhance(origin, destiny, formating, optimal, image_quality):
+def enhance(origin, destiny, formating, optimal, image_quality, keep_original, mode=''):
     # Verify if Image can be openned by PIL. If not, returns 0.
     try:
         process = Image.open(origin)
     except IOError:
         return 0
 
-    # Check image mode in case it needs a change
-    right_mode = check_mode(process.mode, formating)
-    if process.mode != right_mode:
-        if process.mode == 'P':
-            process = process.convert("RGBA")
-            process2 = Image.new('RGBA', process.size, (255, 255, 255))
-            process = Image.alpha_composite(process2, process)
-            if right_mode != 'RGBA':
+    if mode in ['RGB', 'RGBA', 'L', 'P', '1', 'LAB', 'CMYK', 'LA', 'YCbCr', 'I', '']:
+
+        # Check image mode in case it needs a change
+        right_mode = check_mode(process.mode, formating)
+        couldbe_mode = check_mode(mode, formating)
+
+        if couldbe_mode != 0 and couldbe_mode != right_mode:
+            print("Chosen mode not supported by extension, choosing an alternative")
+
+        if process.mode != right_mode:
+            if process.mode == 'P' or process.mode == 'RGBA':
+                process = process.convert("RGBA")
+
+                new_image = Image.new("RGBA", process.size, "WHITE")
+                new_image.paste(process, (0, 0), process)
+                process = new_image
+
+                if right_mode != 'RGBA':
+                    process = process.convert(right_mode)
+            else:
                 process = process.convert(right_mode)
-        else:
-            process = process.convert(right_mode)
+    elif mode != '':
+        print('Mode did not exist')
+        return 0
 
     process.save(destiny, optimize=optimal, quality=image_quality)
 
@@ -28,10 +45,12 @@ def enhance(origin, destiny, formating, optimal, image_quality):
     extention = origin.rpartition('.')[2]
     if extention != destiny.rpartition('.')[2]:
         print(origin, "\tConverted from " + extention + " to " + formating + " and enhanced!")
-        return 1
     else:
         check_size(origin, destiny)
-        return 2
+
+    if not keep_original:
+        os.remove(origin)
+    return 1
 
 
 # Set proper extention to the end result.
@@ -78,22 +97,122 @@ def check_mode(mode, formating):
     # Follows a hierarchy to find easier mode (in the future this could be an option).
     if formating in RGB:
         return 'RGB'
-    if formating in RGBA:
+    elif formating in RGBA:
         return 'RGBA'
-    if formating in P:
+    elif formating in P:
         return 'P'
-    if formating in One:
+    elif formating in One:
         return '1'
-    if formating in L:
+    elif formating in L:
         return 'L'
-    if formating in LAB:
+    elif formating in LAB:
         return 'LAB'
-    if formating in CMYK:
+    elif formating in CMYK:
         return 'CMYK'
-    if formating in LA:
+    elif formating in LA:
         return 'LA'
-    if formating in YCbCr:
+    elif formating in YCbCr:
         return 'YCbCr'
-    if formating in I:
+    elif formating in I:
         return 'I'
     return 0
+
+
+# Image resizer
+def beautifying(origin, width_min, height_min):
+    # Verify if Image can be openned by PIL. If not, returns 0.
+    try:
+        process = Image.open(origin)
+    except IOError:
+        return 0
+
+    # Get pixel data and image size
+    pixdata = process.load()
+    width, height = process.size
+    width -= 1
+    height -= 1
+
+    up, down, left, right = (0, 0), (0, 0), (0, 0), (0, 0)
+
+    # Color of the first pixel
+    first = pixdata[0, 0]
+    last = pixdata[width, height]
+
+    # up
+    for y in range(0, height):
+        if up[0] != 0 or up[1] != 0:
+            break
+        for x in range(0, width):
+            if pixdata[x, y] != first:
+                up = (x, y)
+                break
+    # down
+    for y in range(height, 0, -1):
+        if down[0] != 0 or down[1] != 0:
+            break
+        for x in range(width, 0, -1):
+            if pixdata[x, y] != last:
+                down = (x, y)
+                break
+    # left
+    for x in range(0, width):
+        if left[0] != 0 or left[1] != 0:
+            break
+        for y in range(0, height):
+            if pixdata[x, y] != first:
+                left = (x, y)
+                break
+    # right
+    for x in range(width, 0, -1):
+        if right[0] != 0 or right[1] != 0:
+            break
+        for y in range(height, 0, -1):
+            if pixdata[x, y] != last:
+                right = (x, y)
+                break
+
+    # Get actual proportions of the image disregarding background
+    actual_height = down[1] - up[1]
+    actual_width = right[0] - left[0]
+
+    # Check if image is centered, crop borders based on a ratio with actual_proportions, resize it if less than desired.
+    off_up = up[1]
+    off_down = height - down[1]
+    off_left = left[0]
+    off_right = width - right[0]
+
+    height_border = int(actual_height*0.1)
+    width_border = int(actual_width*0.1)
+
+    if off_up > height_border and off_down > height_border:
+        up = up[1] - height_border
+        down = down[1] + height_border
+    else:
+        up = 0
+        down = height
+    if off_left > width_border and off_right > width_border:
+        left = left[0] - width_border
+        right = right[0] + width_border
+    else:
+        left = 0
+        right = width
+
+    if (up != 0 and down != height) or (left != 0 and right != width):
+        process = process.crop((left, up, right, down))
+        print(origin + " was cropped!")
+
+    actual_ratio = 1
+    if actual_width < width_min or actual_height < height_min:
+        ratio_1 = width_min/actual_width
+        ratio_2 = height_min/actual_height
+        if ratio_1 > ratio_2:
+            actual_ratio = ratio_1
+        else:
+            actual_ratio = ratio_2
+
+    width, height = process.size
+
+    if actual_ratio > 1:
+        process = process.resize((int(width*actual_ratio), int(height*actual_ratio)), Image.ANTIALIAS)
+        print(origin + " was resized!")
+    process.save(origin)
